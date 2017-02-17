480d2f93ad813c73a4820d5d521b27e7ee7ca748
Alexander Wong <admin@alexander-wong.com>
2017-02-16 17:06:46 -0700

    Added relationship between user and videos

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -2,18 +2,7 @@ defmodule Rumbl.UserController do
   alias Rumbl.User
 
   use Rumbl.Web, :controller
-  plug :authenticate when action in [:index, :show]
-
-  defp authenticate(conn, _opts) do
-    if conn.assigns.current_user do
-      conn
-    else
-      conn
-      |> put_flash(:error, "You must be logged in to access that page")
-      |> redirect(to: page_path(conn, :index))
-      |> halt()
-    end
-  end
+  plug :authenticate_user when action in [:index, :show]
 
   def index(conn, _params) do
     users = Repo.all(User)
